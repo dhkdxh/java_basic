6b22c68fb6fd2b4fcb8cfe261e3549c527a2b6f5
dhkdxh <popsicle0404@naver.com>
2025-07-25 12:30:34 +0900

    temp day04

---
+++ src/day04/jungol/자가진단2_2.java
@@ -0,0 +1,16 @@
+package day04.jungol;
+
+public class 자가진단2_2 {
+    public static void main(String[] args) {
+        for(int i=65; i<91; i++){
+            char c = (char)i;
+            System.out.print(c);
+        }
+
+//        int ch = 65;
+//        for(int i=1; i<=26; i++){
+//            System.out.printf("%c", ch);
+//            ch += i;
+//        }
+    }
+}
