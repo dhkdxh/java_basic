db0d28acb29ffdc2285e717bd17cca98bee6aa31
dhkdxh <popsicle0404@naver.com>
2025-07-28 15:39:46 +0900

    day05-3

--- src/day05/jungol/반복제어문3/연습문제6.java
+++ src/day05/jungol/반복제어문3/연습문제6.java
@@ -1,18 +1,16 @@
 package day05.jungol.반복제어문3;
+import java.util.*;
 
 public class 연습문제6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int num1 = 1;
-        char c = 97;
-        for(int i=1; i<=4; i++){
+        int num = sc.nextInt();
+        int n=1;
+        for(int i=1; i<=num; i++){
             for(int j=1; j<=i; j++){
-                System.out.print(c + " ");
-                c++;
-            }
-            for(int j=1; j<=5-i; j++){
-                System.out.print(num1 + " ");
-                num1++;
+                System.out.print(n + " ");
+                n++;
             }
             System.out.println();
         }
---
+++ src/day05/jungol/반복제어문3/형성평가1.java
@@ -0,0 +1,5 @@
+package day05.jungol.반복제어문3;
+import java.util.*;
+
+public class 형성평가1 {
+}
