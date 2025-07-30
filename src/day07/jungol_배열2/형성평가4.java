98e5b500a58e45525e4068fad26fc308b424452a
dhkdxh <popsicle0404@naver.com>
2025-07-30 16:33:15 +0900

    day07-5

---
+++ src/day07/jungol_배열2/형성평가4.java
@@ -0,0 +1,17 @@
+package day07.jungol_배열2;
+
+public class 형성평가4 {
+    public static void main(String[] args) {
+        int[][] arr = {{3, 5, 9}, {2, 11, 5}, {8, 30, 10}, {22, 5, 1}};
+
+        int sum = 0;
+        for(int i=0; i<4; i++){
+            for(int j=0; j<3; j++){
+                System.out.print(arr[i][j]+" ");
+                sum += arr[i][j];
+            }
+            System.out.println();
+        }
+        System.out.println(sum);
+    }
+}
