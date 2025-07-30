dfab39edb09c89fb62ab1b9c83fe08e778dac4d7
dhkdxh <popsicle0404@naver.com>
2025-07-30 14:14:58 +0900

    day07-3

---
+++ src/day06/jungol_배열/형성평가10.java
@@ -0,0 +1,21 @@
+package day06.jungol_배열;
+import java.util.*;
+
+public class 형성평가10 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=n-1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/day06/jungol_배열/형성평가9.java
@@ -0,0 +1,28 @@
+package day06.jungol_배열;
+import java.util.*;
+
+public class 형성평가9 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] arr = new int[100];
+        int index = 0;
+
+        while(true){
+            int input = sc.nextInt();
+            if(input == 0) break;
+
+            arr[index] = input;
+            if(input%2==0){
+                arr[index] /= 2;
+            }else{
+                arr[index] *= 2;
+            }
+            index++;
+        }
+
+        System.out.println(index-1);
+        for(int i=0; i<index; i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+}
