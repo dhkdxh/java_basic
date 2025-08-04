8397a677eee726c261e17b7d11a0215f32d3a72f
dhkdxh <popsicle0404@naver.com>
2025-08-04 16:34:00 +0900

    day10-2

--- src/종합실습문제.java
+++ src/종합실습문제.java
@@ -4,8 +4,12 @@ public class 종합실습문제 {
     static Scanner sc = new Scanner(System.in);
     static String name;
     static String phone;
+    static final int NUM_BOOK = 3;
+    static final int NUM_ITEM = 7;
 
     public static void main(String[] args) {
+
+        String[][] mBook = new String[NUM_BOOK][NUM_ITEM];
 
         System.out.print("당신의 이름을 입력하세요 : ");
         name = sc.nextLine();
@@ -29,7 +33,7 @@ public class 종합실습문제 {
                     menuCartClear();
                     break;
                 case 4:
-                    menuCartAddItem();
+                    menuCartAddItem(mBook);
                     break;
                 case 5:
                     menuCartRemoveItemCount();
@@ -59,7 +63,42 @@ public class 종합실습문제 {
     public static void menuCartClear() {
 
     }
-    public static void menuCartAddItem() {
+    public static void menuCartAddItem(String[][] book) {
+        System.out.println("장바구니에 항목 추가하기 : ");
+        BookList(book);
+        for (int i = 0; i < NUM_BOOK; i++) {
+            for (int j = 0; j < NUM_ITEM; j++) {
+                System.out.print(book[i][j]+" | ");
+            }
+            System.out.println();
+        }
+
+        while(true){
+            System.out.print("장바구니에 추가할 도서의 ID를 입력하세요 :");
+            String input = sc.nextLine();
+
+            int numId = -1;
+            boolean flag=false;
+
+            for(int i=0; i<NUM_BOOK; i++){
+                if(input.equals(book[i][0])){
+                    numId = i;
+                    flag=true;
+                    break;
+                }
+            }
+
+            if(flag){
+                System.out.println("장바구니에 추가하겠습니까? Y | N");
+                input = sc.nextLine();
+
+                if(input.toUpperCase().equals("Y")){
+                    System.out.println(book[numId][0] + " 도서가 장바구니에 추가되었습니다.");
+                }
+                break;
+            }else System.out.println("다시 입력해주세요.");
+
+        }
 
     }
     public static void menuCartRemoveItemCount() {
@@ -86,5 +125,32 @@ public class 종합실습문제 {
         System.out.println(" 7. 영수증 표시하기          8. 종료");
         System.out.println("****************************************************");
         System.out.print("메뉴 번호를 선택해주세요 ");
+    }
+
+    public static void BookList(String[][] book){
+
+        book[0][0] = "ISBN1234";
+        book[0][1] = "쉽게 배우는 JSP 웹 프로그래밍";
+        book[0][2] = "27000";
+        book[0][3] = "송미영";
+        book[0][4] = "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍";
+        book[0][5] = "IT전문서";
+        book[0][6] = "2018/10/08";
+
+        book[1][0] = "ISBN1235";
+        book[1][1] = "안드로이드 프로그래밍";
+        book[1][2] = "33000";
+        book[1][3] = "우재남";
+        book[1][4] = "실습 단계별 명쾌한 멘토링!";
+        book[1][5] = "IT전문서";
+        book[1][6] = "2022/01/22";
+
+        book[2][0] = "ISBN1236";
+        book[2][1] = "스크래치";
+        book[2][2] = "22000";
+        book[2][3] = "고광일";
+        book[2][4] = "컴퓨팅 사고력을 키우는 블록 코딩";
+        book[2][5] = "컴퓨터입문";
+        book[2][6] = "2019/06/10";
     }
 }
