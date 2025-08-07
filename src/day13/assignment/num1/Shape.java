68d10ddf1290483b2aee35ff7288e5151a622cfb
dhkdxh <popsicle0404@naver.com>
2025-08-07 17:46:39 +0900

    day13-2

---
+++ src/day13/assignment/num1/Shape.java
@@ -0,0 +1,37 @@
+package day13.assignment.num1;
+
+public abstract class Shape {
+    protected double area;
+    private String name;
+
+    public Shape(){
+        this.area = 0.0;
+        this.name = "";
+    }
+
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public abstract void calculationArea();
+
+    public void print(){
+        System.out.print("면적은 "+ this.area);
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/day13/assignment/num2/Tea.java
@@ -0,0 +1,27 @@
+package day13.assignment.num2;
+
+public class Tea extends Beverage{
+    static int amount;
+
+    public Tea(String name){
+        super(name);
+        amount++;
+        calcPrice();
+    }
+
+    public void calcPrice(){
+        String menu = this.getName();
+
+        if(menu.equals("lemonTea")){
+            setPrice(1500);
+        }else if(menu.equals("ginsengTea")){
+            setPrice(2000);
+        }else if(menu.equals("redginsengTea")){
+            setPrice(2500);
+        }
+    }
+
+    public void print(){
+        System.out.printf("판매 음료는 %s이며, 가격은 %d\n", this.getName(), this.price);
+    }
+}
