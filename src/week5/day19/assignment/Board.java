f5246e2ce85e15c090021d1e225e50466de00e46
yewonjeon <popsicle0404@naver.com>
2025-08-19 20:35:57 +0900

    day19-final

--- src/week5/day19/assignment/Board.java
+++ src/week5/day19/assignment/Board.java
@@ -1,14 +1,75 @@
 package week5.day19.assignment;
 
-import lombok.Data;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
-@Data
 public class Board {
     private int bno;
     private String bTitle;
     private String bContent;
+
+    private String bwriter;
     private Date bDate;
 
+    public Board(int no, String title, String content, String writer){
+        this.bno = no;
+        this.bTitle = title;
+        this.bContent = content;
+        this.bwriter = writer;
+        bDate = new Date();
+    }
 
+    public void printBoard(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        String date = sdf.format(this.bDate);
+        System.out.println(this.bno + "\t" + this.bwriter + "\t"+date + "\t"+this.bTitle);
+    }
+
+    public void readBoard(){
+        System.out.println("################");
+        System.out.println("번호: "+ this.bno);
+        System.out.println("제목: "+ this.bTitle);
+        System.out.println("내용: "+ this.bContent);
+        System.out.println("작성자: "+this.bwriter);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        String date = sdf.format(this.bDate);
+        System.out.println("날짜: "+date);
+        System.out.println("################");
+    }
+
+    public int getBno() {
+        return bno;
+    }
+
+    public void setBno(int bno) {
+        this.bno = bno;
+    }
+
+    public String getbTitle() {
+        return bTitle;
+    }
+
+    public void setbTitle(String bTitle) {
+        this.bTitle = bTitle;
+    }
+
+    public String getbContent() {
+        return bContent;
+    }
+
+    public void setbContent(String bContent) {
+        this.bContent = bContent;
+    }
+
+    public String getBwriter() {
+        return bwriter;
+    }
+
+    public void setBwriter(String bwriter) {
+        this.bwriter = bwriter;
+    }
+
+    public void setbDate(Date bDate) {
+        this.bDate = bDate;
+    }
 }
--- src/week5/day19/assignment/BoardExample.java
+++ src/week5/day19/assignment/BoardExample.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 public class BoardExample {
     static Scanner sc = new Scanner(System.in);
-    List<Board> boards = new ArrayList<>();
+    static List<Board> boards = new ArrayList<>();
+    static int boardCount = 1;
 
-    public static void list(){
-        System.out.println("------------------------------------------");
-        System.out.println("no\twriter  \tdate   \ttitle");
-        System.out.println("1\twinter  \t2024.01.02   \t게시판에 오신 것을 환영합니다.");
-        System.out.println("2\twinter  \t2024.01.01   \t올 겨울은 많이 춥습니다.");
-        System.out.println();
-        System.out.println("------------------------------------------");
+    public static void main(String[] args) {
+        while(true){
+            list();
+            mainMenu();
+        }
     }
 
     public static void mainMenu(){
+        System.out.println("------------------------------------------");
         System.out.println("메인메뉴: 1. Create | 2. Read | 3. Clear | 4. Exit");
         System.out.print("메뉴 선택: ");
         int input= Integer.parseInt(sc.nextLine());
@@ -22,32 +22,113 @@ public class BoardExample {
         switch(input){
             case 1 -> create();
             case 2 -> read();
-            case 3 -> Clear();
-            case 4 -> System.exit(0);
+            case 3 -> clear();
+            case 4 -> {
+                System.out.println("** 게시판 종료 **");
+                System.exit(0);
+            }
         }
     }
 
     public static void create(){
         System.out.println("[새 개시물 입력]");
-        System.out.println("제목: ");
+        System.out.print("제목: ");
         String title = sc.nextLine();
-        System.out.println("내용: ");
+        System.out.print("내용: ");
         String content = sc.nextLine();
-        System.out.println("작성자: ");
+        System.out.print("작성자: ");
         String name = sc.nextLine();
 
         System.out.println("------------------------------------------");
         System.out.println("보조 메뉴 : 1. OK | 2. Cancel");
         System.out.print("메뉴 선택: ");
         int input= Integer.parseInt(sc.nextLine());
+        System.out.println();
 
         switch (input){
             case 1 -> createBoard(title, content, name);
         }
     }
 
-    public static void createBoard(String title, ){
-        Board board = new Board();
+    public static void createBoard(String title, String content, String name){
+        Board board = new Board(boardCount,title, content, name);
+        boards.add(board);
+        boardCount++;
+        list();
     }
 
+    public static void read() {//예외처리
+        System.out.println("[게시물 읽기]");
+        System.out.print("bno: ");
+        int input = Integer.parseInt(sc.nextLine());
+        if(input <=0 || input > boardCount){
+
+        }
+        boards.get(input-1).readBoard();
+        subMenu1(input);
+    }
+
+    public static void subMenu1(int num){
+        System.out.println("------------------------------------------");
+        System.out.println("보조메뉴: 1.Update | 2.Delete | 3.List");
+        System.out.print("메뉴 선택: ");
+        int input = Integer.parseInt(sc.nextLine());
+        System.out.println();
+
+        switch(input){
+            case 1 -> update(num);
+            case 2 -> delete(num);
+        }
+    }
+
+    public static void update(int num){
+        System.out.println("[수정 내용 입력]");
+        System.out.print("제목: ");
+        String reTitle = sc.nextLine();
+        System.out.print("내용: ");
+        String reContent = sc.nextLine();
+        System.out.print("작성자: ");
+        String reWriter = sc.nextLine();
+
+        System.out.println("------------------------------------------");
+        System.out.println("보조 메뉴: 1. Ok | 2. Cancel");
+        System.out.print("메뉴 선택: ");
+        int input = Integer.parseInt(sc.nextLine());
+
+        if(input == 1){
+            boards.get(num-1).setbTitle(reTitle);
+            boards.get(num-1).setbContent(reContent);
+            boards.get(num-1).setBwriter(reWriter);
+        }
+        list();
+    }
+
+    public static void list(){
+        System.out.println("[게시물 목록]");
+        System.out.println("------------------------------------------");
+        System.out.println("no\twriter \tdate  \t\ttitle");
+        System.out.println("------------------------------------------");
+        for(int i =boards.size()-1; i>=0; i--){
+            boards.get(i).printBoard();
+        }
+        System.out.println();
+    }
+
+    public static void delete(int num){
+        boards.remove(num-1);
+        list();
+    }
+
+    public static void clear(){
+        System.out.println("[게시물 전체 삭제]");
+        System.out.println("------------------------------------------");
+        System.out.println("보조 메뉴: 1. Ok | 2. Cancel");
+        System.out.print("메뉴 선택: ");
+        int input = Integer.parseInt(sc.nextLine());
+        System.out.println();
+
+        boards.clear();
+        boards = new ArrayList<>();
+        list();
+    }
 }
