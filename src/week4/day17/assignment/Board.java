6c99f2aafacba0236053daa2e8edfa0c51732d67
yewonjeon <popsicle0404@naver.com>
2025-08-25 17:19:30 +0900

    assignment-add

---
+++ src/week4/day17/assignment/Board.java
@@ -0,0 +1,18 @@
+package week4.day17.assignment;
+
+public class Board {
+    private String title;
+    private String content;
+
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/week4/day17/assignment/ListExample.java
@@ -0,0 +1,12 @@
+package week4.day17.assignment;
+import java.util.*;
+
+public class ListExample {
+    public static void main(String[] args) {
+        BoardDao dao = new BoardDao();
+        List<Board> list = dao.getBoardList();
+        for(Board board:  list){
+            System.out.println(board.getTitle() + "-"+board.getContent());
+        }
+    }
+}
